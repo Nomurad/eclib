8c81b4f791287a89fdbf4227612906154a00c9a1
seiya nomura <nomurad@outlook.jp>
2019-06-06 22:18:54 +0900

    正規化関数を追加

--- eclib/base/population.py
+++ eclib/base/population.py
@@ -1,3 +1,5 @@
+import numpy as np 
+
 class Population(object):
     ''' 解集団
     GAの個体を管理する (デフォルトではシーケンス型)
@@ -38,3 +40,42 @@ class Population(object):
 
     def filled(self):
         return self.capacity and len(self.data) >= self.capacity
+
+    def normalizing(self):
+        fits = self.data
+        indivs = [indiv.data for indiv in fits]
+        print(indivs.__doc__)
+        
+        if indivs[0].evaluated():
+            n_dim = len(indivs[0].get_variable())
+            obj_dim = len(indivs[0].value)
+        else:
+            raise("Indiv has not been evaluated yet.")
+    
+        max_obj_val = np.full(obj_dim, -np.inf)
+        min_obj_val = np.full(obj_dim, np.inf)
+        normalized_value = np.zeros( (len(indivs), obj_dim) )
+
+        for indiv in indivs:
+            for i in range(obj_dim):
+                if indiv.value[i] > max_obj_val[i]:
+                    max_obj_val[i] = indiv.value[i]
+                    print("max:",max_obj_val)
+                if indiv.value[i] < min_obj_val[i]:
+                    min_obj_val[i] = indiv.value[i]
+                    print("min:",min_obj_val)
+        
+        obj_range = max_obj_val - min_obj_val
+        for i, indiv in enumerate(indivs):
+            indiv.wvalue = tuple((indiv.value-min_obj_val)/obj_range)
+
+            for j in range(obj_dim):
+                print( f'{indiv.value[j]:>7.4f}', end=" ")
+            print("| ", end=" ")
+            for j in range(obj_dim):
+                print( f'{indiv.wvalue[j]:>7.4f}', end=" ")
+            print()
+
+        print()
+        print(max_obj_val)
+        print(min_obj_val) 
