68fea19b15effc75abd7ef2b4126faf454c982ff
nomurad <nomurad@outlook.jp>
2019-10-06 19:19:17 +0900

    modify .gitignore

--- build/lib.linux-x86_64-2.7/eclib/benchmarks/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .testfunctions import rosenbrock
-from .testfunctions import zdt1
-from .testfunctions import zdt2
-from .testfunctions import zdt3
-from .testfunctions import zdt4
-from .testfunctions import zdt6
